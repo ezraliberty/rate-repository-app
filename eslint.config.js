a5d918e4f3086daab58495c6a2dfbd6637500ba2
ezraliberty <ezraliberty99@gmail.com>
2025-04-28 13:31:32 +0100

    lint added

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+const eslint = require('@eslint/js');
+const reactPlugin = require('eslint-plugin-react');
+const reactNativePlugin = require('eslint-plugin-react-native');
+const babelParser = require('@babel/eslint-parser');
+const globals = require('globals');
+
+const reactRecommended = reactPlugin.configs.recommended;
+
+module.exports = [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: reactPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...reactRecommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      'react-native': reactNativePlugin,
+    },
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        babelOptions: {
+          presets: ['module:metro-react-native-babel-preset'],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        __DEV__: 'readonly',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-native/no-unused-styles': 'warn',
+      'react-native/split-platform-components': 'warn',
+      'react-native/no-inline-styles': 'warn',
+    },
+  },
+];
